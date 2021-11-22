e8d78eafd513f302a3a30af769296338aecf9644
GhousIqbal <ghousiqbal911@gmail.com>
2021-11-22 16:20:21 +0500

    code

--- project.py
+++ project.py
@@ -7,3 +7,14 @@ for x in range(0, 10):
 print(arr3)
 arrlen = len(arr3)
 print(arrlen)
+print("___________________________")
+
+arr4 = [10, 11, 12, 31, 14, 15, 16, 7, 81, 19]
+arr5 = [91, 81, 71, 16, 5, 114, 3, 21, 11, 0]
+arr6 = []
+for x in range(0, 10):
+    x = arr4[x]+arr5[x]
+    arr6.append(x)
+print(arr6)
+arrlen2 = len(arr6)
+print(arrlen2)
